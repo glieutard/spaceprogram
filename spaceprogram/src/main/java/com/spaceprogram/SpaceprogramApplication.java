fcd33273e5f3e797b8df4f9f92652426e5c21c8e
glieutard <43911638+glieutard@users.noreply.github.com>
2018-10-06 19:21:37 +0200

    Initialisation du projet
    pom
    main class
    propeties

--- spaceprogram/src/main/java/com/spaceprogram/SpaceprogramApplication.java
+++ spaceprogram/src/main/java/com/spaceprogram/SpaceprogramApplication.java
@@ -1,12 +1,20 @@
 package com.spaceprogram;
 
-import org.springframework.boot.SpringApplication;
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJSONDoc
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.spaceprogram")
 public class SpaceprogramApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpaceprogramApplication.class, args);
+//		SpringApplication.run(SpaceprogramApplication.class, args);
+		
+		new SpringApplicationBuilder(SpaceprogramApplication.class).properties("spring.config.name:spaceprogram").build().run(args);
 	}
 }
