7795b21715848bc66d01c0bc583f14db8c83d4d3
glieutard <43911638+glieutard@users.noreply.github.com>
2018-10-06 18:38:40 +0200

    initial commit

---
+++ spaceprogram/src/main/java/com/spaceprogram/SpaceprogramApplication.java
@@ -0,0 +1,12 @@
+package com.spaceprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceprogramApplication.class, args);
+	}
+}
---
+++ spaceprogram/src/test/java/com/spaceprogram/SpaceprogramApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spaceprogram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpaceprogramApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
